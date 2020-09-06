43306ca29f7180a8a6dcce0aaf254fd42c0f6ac0
hamza ali <hamza54834@gmail.com>
2020-09-06 05:05:24 -0700

    olx app working

--- js/app.js
+++
@@ -1,7 +0,0 @@
-var screen=document.getElementById('load-screen');
-var loading=document.getElementById('loading');
-
-window.addEventListener('load',function(){
-    screen.style.fadeOut='slow';
-    screen.style.display= 'none';
-})
